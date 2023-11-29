ebef97e659d8dfc1f3c70488fe80b406cf202f16
harshpa324 <harshpa324@gmail.com>
2023-11-30 02:25:15 +0530

    added start, navbar, and a bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
-import Image from 'next/image'
-
+import Start from "./components/Start"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 export default function Home() {
-  return<>
-  Hello, I am Harsh Pal
-  </>
+  return (
+    <main className="flex min-h-screen flex-col bg-[#000000]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <Start />
+      </div>
+      <Footer />
+    </main>
+  )
 }
